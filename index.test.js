a6bcf13a3813c59487a2f8b835e9cc58667fe729
Mohammed Akif Salah <65378412+mas123786@users.noreply.github.com>
2023-02-16 20:01:42 +0530

    commited

--- index.test.js
+++ index.test.js
@@ -2,8 +2,8 @@ const request = require('supertest');
 const app = require('./index');
 
 describe('GET /', () => {
-  it('responds with "Servers is Running"', async () => {
+  it('responds with "Server is Running"', async () => {
     const response = await request(app).get('/');
-    expect(response.text).toEqual('Servers is Running');
+    expect(response.text).toEqual('Server is Running');
   });
 });
