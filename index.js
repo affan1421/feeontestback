1791ee485083a5cbd5b6f0f6da3d6184b253c73d
Mohammed Akif Salah <65378412+mas123786@users.noreply.github.com>
2023-02-16 15:16:20 +0530

    commited

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Servers is Running');
+  res.send('Server is Running.....');
 });
 
 const port = process.env.PORT || 3000;
--- index.test.js
+++ index.test.js
@@ -4,6 +4,6 @@ const app = require('./index');
 describe('GET /', () => {
   it('responds with "Server is Running"', async () => {
     const response = await request(app).get('/');
-    expect(response.text).toEqual('Server is Running');
+    expect(response.text).toEqual('Server is Running.....');
   });
 });
