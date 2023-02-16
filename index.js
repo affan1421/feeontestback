f8f54772d9d8c696993f9fba2e97393f40fabfd1
Mohammed Akif Salah <65378412+mas123786@users.noreply.github.com>
2023-02-16 18:59:24 +0530

    Yml Updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Server is Running');
+  res.send('Server is Running.....');
 });
 
 const port = process.env.PORT || 3000;
