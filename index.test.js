0fb50b62b780f471c9ae9037858166b458337ba7
Mohammed Akif Salah <65378412+mas123786@users.noreply.github.com>
2023-02-17 16:00:45 +0530

    Action Fixed

--- index.test.js
+++ index.test.js
@@ -2,8 +2,8 @@ const request = require('supertest');
 const app = require('./index');
 
 describe('GET /', () => {
-  it('responds with "Servers is Running"', async () => {
+  it('responds with "Server is up and Running👨‍💻👩‍💻"', async () => {
     const response = await request(app).get('/');
-    expect(response.text).toEqual('Servers is Running');
+    expect(response.text).toEqual('Server is up and Running👨‍💻👩‍💻');
   });
 });
