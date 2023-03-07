d46aeb75a00678dc2aa474c8fa07820b9b2453d0
Nawaz Pasha <nawaz22022001@gmail.com>
2023-03-07 15:59:29 +0530

    Fee Structure Model

---
+++ models/feeStructure.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const feeDetailSchema = new Schema({
+    feeType: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'FeeType',
+        required: true
+    },
+    feeSchedule: {
+        type: String,
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    breakdown: {
+        type: String
+    },
+});
+
+const feeStructureSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    academicYear: {
+        type: String,
+        required: true
+    },
+    class: {
+        type: String,
+        required: true
+    },
+    feeDetails: [feeDetailSchema],
+    totalAmount: {
+        type: Number,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('FeeStructure', feeStructureSchema);
