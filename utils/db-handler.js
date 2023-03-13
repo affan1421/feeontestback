169fa92e3ca82ac46c142636be09211bfeb27e28
hafizikram <ikram.cumin@gmail.com>
2023-03-13 14:30:56 +0530

    e2e test added.

--- utils/db-handler.js
+++ utils/db-handler.js
@@ -1,31 +1,31 @@
-/* eslint-disable guard-for-in */
-const mongoose = require('mongoose');
-const { MongoMemoryServer } = require('mongodb-memory-server');
+// /* eslint-disable guard-for-in */
+// const mongoose = require('mongoose');
+// const { MongoMemoryServer } = require('mongodb-memory-server');
 
-const mongod = new MongoMemoryServer();
+// let mongod = null;
 
-module.exports.connectDatabase = async () => {
-	const uri = await mongod.getUri();
+// mongod = new MongoMemoryServer();
+// const connectDatabase = async () => {
+// 	try {
+// 		const uri = await mongod.getUri();
+// 		const mongooseOpts = {
+// 			useNewUrlParser: true,
+// 			useUnifiedTopology: true,
+// 		};
+// 		await mongoose.connect(uri, mongooseOpts);
+// 		console.log('Database Connected');
+// 	} catch (error) {
+// 		console.error('Failed to connect to the database:', error);
+// 	}
+// };
 
-	const mongooseOpts = {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	};
+// const closeDatabase = async () => {
+// 	await mongoose.connection.dropDatabase();
+// 	await mongoose.connection.close();
+// 	await mongod.stop();
+// };
 
-	await mongoose.connect(uri, mongooseOpts);
-};
-
-module.exports.closeDatabase = async () => {
-	await mongoose.connection.dropDatabase();
-	await mongoose.connection.close();
-	await mongod.stop();
-};
-
-module.exports.clearDatabase = async () => {
-	const { collections } = mongoose.connection;
-
-	for (const key in collections) {
-		const collection = collections[key];
-		await collection.deleteMany();
-	}
-};
+// module.exports = {
+// 	connectDatabase,
+// 	closeDatabase,
+// };
