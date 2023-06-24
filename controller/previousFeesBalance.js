319067265e1dec2f6338937feba3d9f40e43de36
hafizikram <ikram.cumin@gmail.com>
2023-06-24 15:00:42 +0530

    Previous Balance APIs (Creation and Get By Filter) and expense date format

--- controller/previousFeesBalance.js
+++ controller/previousFeesBalance.js
@@ -1,14 +1,68 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable prefer-destructuring */
+const mongoose = require('mongoose');
+const moment = require('moment');
 const PreviousBalance = require('../models/previousFeesBalance');
 const SuccessResponse = require('../utils/successResponse');
 const ErrorResponse = require('../utils/errorResponse');
 const CatchAsync = require('../utils/catchAsync');
 
-const GetAllByFilter = async (req, res) => {};
+const Student = mongoose.connection.db.collection('students');
+
+const GetAllByFilter = CatchAsync(async (req, res, next) => {
+	let {
+		schoolId,
+		academicYearId,
+		isEnrolled = false,
+		page,
+		limit,
+		sectionId,
+	} = req.query;
+	const payload = {};
+
+	if (schoolId) {
+		payload.schoolId = mongoose.Types.ObjectId(schoolId);
+	} else {
+		return next(new ErrorResponse('Please Provide The School Id', 422));
+	}
+	if (academicYearId) {
+		payload.academicYearId = mongoose.Types.ObjectId(academicYearId);
+	}
+	if (isEnrolled) {
+		payload.isEnrolled = isEnrolled === 'true';
+	}
+	if (sectionId) {
+		payload.sectionId = mongoose.Types.ObjectId(sectionId);
+	}
+	// Optional Pagination
+	const dataFacet = [{ $match: payload }];
+	if (page && limit) {
+		page = +page;
+		limit = +limit;
+		dataFacet.push({ $skip: page * limit }, { $limit: limit });
+	}
+	const previousBalances = await PreviousBalance.aggregate([
+		{
+			$facet: {
+				data: dataFacet,
+				count: [{ $match: payload }, { $count: 'count' }],
+			},
+		},
+	]);
+	const { data, count } = previousBalances[0];
+
+	if (count.length === 0) {
+		return next(new ErrorResponse('No Previous Fee Found', 404));
+	}
+	res
+		.status(200)
+		.json(SuccessResponse(data, count[0].count, 'Fetched Successfully'));
+});
 
 const CreatePreviousBalance = CatchAsync(async (req, res, next) => {
-	const {
+	let {
 		studentId = null,
-		dueDate = new Date(),
+		dueDate = moment().subtract(1, 'days').format('MM-DD-YYYY'),
 		studentName, // Left
 		parentName, // Left
 		username, // Left
@@ -20,37 +74,76 @@ const CreatePreviousBalance = CatchAsync(async (req, res, next) => {
 	} = req.body;
 	const isEnrolled = !!studentId;
 	if (
-		!studentName ||
-		!parentName ||
-		!username ||
-		!gender ||
-		!sectionId ||
-		!pendingAmount ||
+		(!studentId && (!studentName || !parentName || !username || !gender)) ||
+		!academicYearId ||
 		!schoolId ||
-		(gender !== 'Male' && gender !== 'Female') ||
-		(studentName && !studentId) ||
-		(parentName && !studentId) ||
-		(username && !studentId) ||
-		(gender && !studentId)
+		!sectionId ||
+		!pendingAmount
 	) {
 		return next(new ErrorResponse('Please Provide All The Input Fields', 422));
 	}
 
-	const previousBalance = await PreviousBalance.create({
-		studentId,
-		amount,
+	// Fetch student, parent, username and gender from studentId
+	if (studentId) {
+		const student = await Student.aggregate([
+			{
+				$match: {
+					_id: mongoose.Types.ObjectId(studentId),
+				},
+			},
+			{
+				$lookup: {
+					from: 'parents',
+					localField: 'parent_id',
+					foreignField: '_id',
+					as: 'parent',
+				},
+			},
+			{
+				$project: {
+					_id: 0,
+					studentName: '$name',
+					parentName: {
+						$first: '$parent.name',
+					},
+					username: 1,
+					gender: 1,
+				},
+			},
+		]).toArray();
+		({ studentName, parentName, username, gender } = student[0]);
+	}
+
+	// Status
+	// TODO: Configure the cron job to update the status of previous balance
+	const status = moment(dueDate, 'MM-DD-YYYY').isBefore(moment())
+		? 'Due'
+		: 'Upcoming';
+
+	const creationPayload = {
+		dueDate: new Date(dueDate),
 		isEnrolled,
 		studentName,
 		parentName,
-		status,
 		username,
+		status,
 		gender,
+		schoolId,
 		sectionId,
 		academicYearId,
 		pendingAmount,
-	});
+	};
+	studentId ? (creationPayload.studentId = studentId) : null;
 
-	res.status(201).json(new SuccessResponse(201, previousBalance));
+	const previousBalance = await PreviousBalance.create(creationPayload);
+
+	if (!previousBalance) {
+		return next(new ErrorResponse('Unable To Create Previous Balance', 500));
+	}
+
+	res
+		.status(201)
+		.json(SuccessResponse(previousBalance, 1, 'Created Successfully'));
 });
 
 const BulkCreatePreviousBalance = async (req, res) => {};
--- models/previousFeesBalance.js
+++ models/previousFeesBalance.js
@@ -2,63 +2,72 @@ const mongoose = require('mongoose');
 
 const { Schema, model } = mongoose;
 
-const previousBalanceSchema = new Schema({
-	isEnrolled: {
-		type: Boolean,
-		required: true,
+const previousBalanceSchema = new Schema(
+	{
+		isEnrolled: {
+			type: Boolean,
+			required: [true, 'Please Provide The IsEnrolled'],
+		},
+		dueDate: {
+			type: Date,
+			default: Date.now(),
+		},
+		schoolId: {
+			type: Schema.Types.ObjectId,
+			ref: 'School',
+			required: [true, 'Please Provide The School'],
+		},
+		studentId: {
+			type: Schema.Types.ObjectId,
+			ref: 'Student',
+		},
+		studentName: {
+			type: String,
+			required: [true, 'Please Provide The Student Name'],
+		},
+		parentName: {
+			type: String,
+			required: [true, 'Please Provide The Parent Name'],
+		},
+		status: {
+			type: String,
+			required: true,
+			enum: ['Due', 'Paid', 'Upcoming'],
+		},
+		lastPaidDate: Date,
+		username: {
+			type: String,
+			required: [true, 'Please Provide The Mobile Number'],
+		},
+		gender: {
+			type: String,
+			required: [true, 'Please Provide The Gender'],
+			enum: ['Male', 'Female'],
+		},
+		sectionId: {
+			type: Schema.Types.ObjectId,
+			ref: 'Section',
+			required: [true, 'Please Provide The Section'],
+		},
+		academicYearId: {
+			type: Schema.Types.ObjectId,
+			ref: 'AcademicYear',
+			required: [true, 'Please Provide The Academic Year'],
+		},
+		pendingAmount: {
+			type: Number,
+			required: [true, 'Please Provide The Pending Amount'],
+			min: 1,
+		},
+		receiptId: {
+			type: Schema.Types.ObjectId,
+			ref: 'FeeReceipt',
+		},
 	},
-	dueDate: {
-		type: Date,
-		default: Date.now(),
-	},
-	schoolId: {
-		type: Schema.Types.ObjectId,
-		ref: 'School',
-		required: true,
-	},
-	studentId: {
-		type: Schema.Types.ObjectId,
-		ref: 'Student',
-	},
-	studentName: {
-		type: String,
-		required: true,
-	},
-	parentName: {
-		type: String,
-		required: true,
-	},
-	status: {
-		type: String,
-		required: true,
-	},
-	username: {
-		type: String,
-		required: true,
-	},
-	gender: {
-		type: String,
-		required: true,
-	},
-	sectionId: {
-		type: Schema.Types.ObjectId,
-		ref: 'Section',
-		required: true,
-	},
-	academicYearId: {
-		type: Schema.Types.ObjectId,
-		ref: 'AcademicYear',
-		required: true,
-	},
-	pendingAmount: {
-		type: Number,
-		required: true,
-	},
-	receiptId: {
-		type: Schema.Types.ObjectId,
-		ref: 'FeeReceipt',
-	},
-});
+	{
+		timestamps: true,
+	}
+);
 
 const previousFeesBalance = model('previousFeesBalance', previousBalanceSchema);
 
