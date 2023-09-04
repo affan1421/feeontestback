3d643ee0f434c8c96acb4b1b0dd66e597a11b10f
hafizikram <ikram.cumin@gmail.com>
2023-09-04 14:09:52 +0530

    Discount -student fetch bug

--- models/discountStructure.js
+++ models/discountStructure.js
@@ -25,26 +25,31 @@ const feeDetailSchema = new Schema({
 	breakdown: [breakdownSchema],
 });
 
-const discountStructureSchema = new Schema({
-	discountId: {
-		type: Schema.Types.ObjectId,
-		ref: 'DiscountCategory',
-		required: true,
+const discountStructureSchema = new Schema(
+	{
+		discountId: {
+			type: Schema.Types.ObjectId,
+			ref: 'DiscountCategory',
+			required: true,
+		},
+		categoryId: {
+			type: Schema.Types.ObjectId,
+			ref: 'FeeCategory',
+			required: true,
+		},
+		schoolId: { type: Schema.Types.ObjectId, required: true },
+		feeStructureId: {
+			type: Schema.Types.ObjectId,
+			ref: 'FeeStructure',
+			required: true,
+		},
+		totalFeesAmount: Number,
+		feeDetails: [feeDetailSchema],
 	},
-	categoryId: {
-		type: Schema.Types.ObjectId,
-		ref: 'FeeCategory',
-		required: true,
-	},
-	schoolId: { type: Schema.Types.ObjectId, required: true },
-	feeStructureId: {
-		type: Schema.Types.ObjectId,
-		ref: 'FeeStructure',
-		required: true,
-	},
-	totalFeesAmount: Number,
-	feeDetails: [feeDetailSchema],
-});
+	{
+		timestamps: true,
+	}
+);
 
 const DiscountStructure = model('discountStructure', discountStructureSchema);
 
