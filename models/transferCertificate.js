f562619ea89c8f5decc557a840b87a167f0e0ce7
jbsankarlal <jbsankarlal@gmail.com>
2023-09-21 17:38:51 +0530

    schema created

---
+++ models/transferCertificate.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const studentTransferSchema = new Schema(
+  {
+    studentId: {
+      type: Schema.Types.ObjectId,
+      ref: "School",
+      required: [true, "studentId is required"],
+    },
+    class: {
+      type: Schema.Types.ObjectId,
+      ref: "Class",
+      required: [true, "class is required"],
+    },
+    tcType: {
+      type: String,
+      enum: ["ALUMINII-TC", "AVAIL-TC", "BLOCKED"],
+      required: [true, "Type is required"],
+    },
+    reason: String,
+    transferringSchool: String,
+    status: {
+      type: String,
+      enum: ["PENDING", "APPROVED", "REJECTED"],
+      default: "PENDING",
+    },
+  },
+  { timestamps: true }
+);
+
+const StudentTransfer = model("StudentTransfer", studentTransferSchema);
+
+module.exports = StudentTransfer;
