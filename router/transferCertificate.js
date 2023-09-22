bb14999a2b91d519187e90cca572a5f6470beef1
jbsankarlal <jbsankarlal@gmail.com>
2023-09-22 15:38:00 +0530

    conflict resolved

--- router/transferCertificate.js
+++ router/transferCertificate.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 const {
-<<<<<<< HEAD
   createStudentTransfer,
   getUsers,
   changeStatus,
   viewAttachments,
+  getTc,
+  getTcDetails,
 } = require("../controller/transferCertificate");
 
 // create new tranfer certificate
@@ -12,19 +13,11 @@ router.post("/", createStudentTransfer);
 router.get("/users", getUsers);
 router.put("/changeStatus", changeStatus);
 router.get("/attachments/:studentTransferId", viewAttachments);
-=======
-	createStudentTransfer,
-	getUsers,
-	changeStatus,
-	getTc,
-	getTcDetails
-} = require('../controller/transferCertificate');
 
 // create new tranfer certificate
-router.post('/', createStudentTransfer);
-router.get('/users', getUsers);
-router.put('/changeStatus', changeStatus);
-router.get('/tcList', getTc);
-router.get('/details', getTcDetails)
->>>>>>> 3b815d60b4b6259dc93714fa3a68db2c3b26fb31
+router.post("/", createStudentTransfer);
+router.get("/users", getUsers);
+router.put("/changeStatus", changeStatus);
+router.get("/tcList", getTc);
+router.get("/details", getTcDetails);
 module.exports = router;
