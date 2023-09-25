6256083f09a573caaa77a34aa383a45ff31414fb
jbsankarlal <jbsankarlal@gmail.com>
2023-09-25 13:29:18 +0530

    route modiff

--- router/transferCertificate.js
+++ router/transferCertificate.js
@@ -1,18 +1,7 @@
 const router = require("express").Router();
 const {
-<<<<<<< Updated upstream
-	createStudentTransfer,
-	searchStudentsWithPagination,
-	changeStatus,
-	viewAttachments,
-	getTc,
-	getTcDetails,
-	getClasses,
-	getTcStudentsDetails,
-} = require('../controller/transferCertificate');
-=======
   createStudentTransfer,
-  getStudents,
+  searchStudentsWithPagination,
   changeStatus,
   viewAttachments,
   getTc,
@@ -20,13 +9,12 @@ const {
   getClasses,
   getTcStudentsDetails,
 } = require("../controller/transferCertificate");
->>>>>>> Stashed changes
 
 // create new tranfer certificate
 router.post("/", createStudentTransfer);
 
 // view all students of a particular school
-router.get("/students", getStudents);
+router.get("/students", searchStudentsWithPagination);
 
 // change the TC status
 router.put("/changeStatus/:id", changeStatus);
@@ -46,15 +34,4 @@ router.get("/attachments/:studentTransferId", viewAttachments);
 // in-detail data of students who applied for TC
 router.get("/tcStudentsDetails", getTcStudentsDetails);
 
-<<<<<<< Updated upstream
-// create new tranfer certificate
-router.post('/', createStudentTransfer);
-router.get('/students', searchStudentsWithPagination);
-router.put('/changeStatus/:id', changeStatus);
-router.get('/tcList', getTc);
-router.get('/details', getTcDetails);
-router.get('/classes', getClasses);
-router.get('/tcStudentsDetails', getTcStudentsDetails);
-=======
->>>>>>> Stashed changes
 module.exports = router;
