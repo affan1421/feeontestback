d30284ed5a6ce870c47bd9cd728b6946a623032b
hafizikram <ikram.cumin@gmail.com>
2023-10-06 18:56:04 +0530

    ESLint formatting.

--- router/dailyCloseCollection.js
+++ router/dailyCloseCollection.js
@@ -1,18 +1,18 @@
-const router = require("express").Router();
+const router = require('express').Router();
 const {
-  generateDailyCloseCollection,
-  getCollectionDetails,
-  dailyTotalFeeCollection,
-  updateCloseCollectionStatus,
-  getEditStatus,
-  updateEditStatus,
-} = require("../controller/dailyCloseCollection");
+	generateDailyCloseCollection,
+	getCollectionDetails,
+	dailyTotalFeeCollection,
+	updateCloseCollectionStatus,
+	getEditStatus,
+	updateEditStatus,
+} = require('../controller/dailyCloseCollection');
 
-router.post("/create", generateDailyCloseCollection);
-router.get("/collectionDetails", getCollectionDetails);
-router.get("/todaystotalfees", dailyTotalFeeCollection);
-router.post("/updateStatus", updateCloseCollectionStatus);
-router.put("/allowEdit", updateEditStatus);
-router.get("/allowEdit", getEditStatus);
+router.post('/create', generateDailyCloseCollection);
+router.get('/collectionDetails', getCollectionDetails);
+router.get('/todaystotalfees', dailyTotalFeeCollection);
+router.post('/updateStatus', updateCloseCollectionStatus);
+router.put('/allowEdit', updateEditStatus);
+router.get('/allowEdit', getEditStatus);
 
 module.exports = router;
