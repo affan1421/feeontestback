6d5a8edcdba1e4de195081e4259800c26a5a4b58
mhdirfanpn <mhdirfanpn@gmail.com>
2023-10-16 10:55:13 +0530

    concession classList update

--- router/concession.js
+++ router/concession.js
@@ -4,25 +4,20 @@ const {
   getClassDetails,
   getStudentsByClass,
   getStudentFeeDetails,
-<<<<<<< Updated upstream
   getConcessionCardData,
   getConcessionClassList,
-  changeStatus
-=======
   getStudentConcessionData,
->>>>>>> Stashed changes
+  changeStatus,
 } = require("../controller/concession");
 
 router.post("/create", createConcession);
 router.get("/classes", getClassDetails);
 router.get("/classWiseStudentsData", getStudentsByClass);
 router.get("/feedetails", getStudentFeeDetails);
-<<<<<<< Updated upstream
 router.get("/concessionCardData",getConcessionCardData)
 router.get("/concessionClassList",getConcessionClassList)
 router.get("/changeStatus",changeStatus);
-=======
 router.get("/studentsconcession", getStudentConcessionData);
->>>>>>> Stashed changes
+
 
 module.exports = router;
