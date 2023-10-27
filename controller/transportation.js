b656692d32410511b589d8aef1dc69aa8055f893
jbsankarlal <jbsankarlal@gmail.com>
2023-10-27 16:56:42 +0530

    route search and listing api

--- controller/transportation.js
+++ controller/transportation.js
@@ -26,6 +26,35 @@ const createNewRoute = async (req, res, next) => {
   }
 };
 
+const getRoutes = async (req, res, next) => {
+  try {
+    const { schoolId, searchQuery, page, limit } = req.query;
+
+    const pageNumber = parseInt(page) || 1;
+    const pageSize = parseInt(limit) || 5;
+    const skip = (pageNumber - 1) * pageSize;
+
+    const query = {
+      schoolId: mongoose.Types.ObjectId(schoolId),
+    };
+
+    if (searchQuery) {
+      query.$or = [
+        { routeName: { $regex: searchQuery, $options: "i" } },
+        { startingPoint: { $regex: searchQuery, $options: "i" } },
+      ];
+    }
+
+    const routes = await BusRoute.find(query).skip(skip).limit(pageSize);
+
+    res.status(200).json(SuccessResponse(routes, routes.length, "Fetched Successfully"));
+  } catch (error) {
+    console.log("error", error.message);
+    return next(new ErrorResponse("Something Went Wrong", 500));
+  }
+};
+
 module.exports = {
   createNewRoute,
+  getRoutes,
 };
--- router/transportation.js
+++ router/transportation.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
-const { createNewRoute } = require("../controller/transportation");
+const { createNewRoute, getRoutes } = require("../controller/transportation");
 
 router.post("/createRoute", createNewRoute);
+router.get("/searchRoute", getRoutes);
 
 module.exports = router;
