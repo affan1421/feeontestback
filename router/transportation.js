18342799cdf337de69d60e932fdaf7b9de27c8a5
jbsankarlal <jbsankarlal@gmail.com>
2023-10-28 11:15:09 +0530

    api modified

--- router/transportation.js
+++ router/transportation.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
+
 const { createNewRoute, getRoutes, editRoutes } = require("../controller/transportation");
+
 router.post("/createRoute", createNewRoute); // creating new routes
 router.get("/searchRoute", getRoutes); // listing routes and searching on route name
 router.post("/editRoutes", editRoutes); // edit the existing routes
